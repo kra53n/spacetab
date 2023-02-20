f701286d3a1833e5c2782268224ca903310fcdda
Krai53n <greasha46@gmail.com>
2023-02-20 22:50:00 +0300

    1st commit

---
+++ stapcetab.el
@@ -0,0 +1,13 @@
+;;; Functionalty:
+;; - spacetab-tab-indent-size
+;; - spacetab-tabme
+;; - spacetab-space-indet-size
+;; - spacetab-spaceme
+;;; me functions:
+;; -region
+;; -line
+
+(defun spacetab-define-entity ()
+  (message "hello"))
+
+(spacetab-define-entity)
